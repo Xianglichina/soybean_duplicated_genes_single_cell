1733fe8bf11f8437ad99e169c391ef3146862fe4
Xianglichina <31661572+Xianglichina@users.noreply.github.com>
2025-07-02 10:49:56 -0400

    Update make_networks_r.sh

--- step3_divergence_expression_pattern_within_tissue/make_networks_r.sh
+++ step3_divergence_expression_pattern_within_tissue/make_networks_r.sh
@@ -7,7 +7,7 @@
 #SBATCH --output=r_script.%j.out    # Standard output log
 #SBATCH --error=r_script.%j.err     # Standard error log
 #SBATCH --mail-type=END,FAIL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
-#SBATCH --mail-user=xl20359@uga.edu  # Where to send mail (change username@uga.edu to your email address
+
 
 module load R/4.3.1-foss-2022a   
 Rscript get_coexp_network_no_filter.r  early_nodule_normalized.csv
